03567290c8e58610f031da200158b6d5cb1b03ec
Wojciech Blachowski <wblachowsky@gmail.com>
2019-01-01 21:40:13 +0100

    Add creating random population

--- src/main/java/com/wblachowski/ptsz/scheduler/sorter/genetic/GeneticSorter.java
+++ src/main/java/com/wblachowski/ptsz/scheduler/sorter/genetic/GeneticSorter.java
@@ -1,15 +1,32 @@
 package com.wblachowski.ptsz.scheduler.sorter.genetic;
 
 import com.wblachowski.ptsz.data.Instance;
+import com.wblachowski.ptsz.data.Job;
 import com.wblachowski.ptsz.scheduler.sorter.Sorter;
 
+import java.util.*;
+
 public class GeneticSorter extends Sorter {
+    private static final int POPULATION_SIZE = 200;
+    private List<List<Job>> population;
+
+
     public GeneticSorter(Instance instance) {
         super(instance);
     }
 
     @Override
     public void sort() {
+        population = generateInitialPopulation();
+    }
 
+    private List<List<Job>> generateInitialPopulation() {
+        List<List<Job>> population = new ArrayList<>();
+        List<Job> allJobs = new ArrayList<>(getJobs());
+        for (int i = 0; i < POPULATION_SIZE; i++) {
+            Collections.shuffle(allJobs);
+            population.add(new ArrayList<>(allJobs));
+        }
+        return population;
     }
 }
